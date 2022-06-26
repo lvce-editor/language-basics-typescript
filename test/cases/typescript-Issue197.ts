ce2b507b87c8d71989528ffaf6d83e06cbb36e1f
Le Vivilet <levivlet@pm.me>
2022-06-26 15:09:24 +0200

    convert files from clrf to lf

--- test/cases/typescript-Issue197.ts
+++ test/cases/typescript-Issue197.ts
@@ -1,11 +1,11 @@
  export class sampleClass  {
         public equipmentTypeList: string[] = ["FAX", "OFFICE PHONE"];
         public pbxEquipmentType = "FAX";
-        public spliceRightStripName = "TE"; 
-        public cablePairStatusValues: any[] =  
-            [       
+        public spliceRightStripName = "TE";
+        public cablePairStatusValues: any[] =
+            [
                 { statusText: "Assigned" },
-                { statusText: "Faulty" }, 
+                { statusText: "Faulty" },
                 { statusText: "Not in Use" },
             ];
         public cablePairStatusValueForAssigned = "Assigned";
