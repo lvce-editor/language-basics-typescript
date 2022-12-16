8e579c600b3686d0ae8d1bf401acc012e30df8d0
Le Vivilet <levivlet@pm.me>
2022-12-16 22:26:53 +0100

    update typescript tests

--- scripts/copy-typescript-tests.sh
+++ scripts/copy-typescript-tests.sh
@@ -5,7 +5,7 @@ cd ..
 
 rm -rf .tmp
 
-COMMIT="00d57af00c715340715c67acf8de56070e9fef08"
+COMMIT="b1ba1b2f8fb58805c3c8b2a3e337e4610340c0b5"
 
 git clone https://github.com/microsoft/TypeScript-TmLanguage .tmp/typescript-tm-language
 
