48b9bdb7fc921e88b16a9923a75f4b626b4381a6
Le Vivilet <levivlet@pm.me>
2023-01-21 23:08:05 +0100

    fix: highlighting of private class property

--- src/tokenizeTypeScript.js
+++ src/tokenizeTypeScript.js
@@ -111,7 +111,7 @@ const RE_LINE_COMMENT = /^\/\/[^\n]*/
 const RE_KEYWORD =
   /^(?:yield|with|while|void|var|typeof|type|true|try|throw|this|static|switch|super|readonly|return|public|protected|private|package|null|new|let|interface|instanceof|in|import|implements|if|function|for|finally|from|false|extends|export|enum|else|do|delete|default|debugger|declare|continue|const|constructor|class|catch|case|break|await|async|abstract)\b/
 const RE_WHITESPACE = /^\s+/
-const RE_VARIABLE_NAME = /^[\$a-zA-Z\_][\$a-zA-Z\_\d]*/
+const RE_VARIABLE_NAME = /^[\#\$a-zA-Z\_][\$a-zA-Z\_\d]*/
 const RE_PUNCTUATION = /^[:,;\{\}\[\]\.=\(\)>\+]/
 const RE_QUOTE_SINGLE = /^'/
 const RE_QUOTE_DOUBLE = /^"/
