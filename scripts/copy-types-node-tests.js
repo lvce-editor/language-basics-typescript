c7ac438c3745be80ace46889f4826894e96598a1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-07-11 15:04:33 +0200

    test: add lots of tests from @types/node (#80)

---
+++ scripts/copy-types-node-tests.js
@@ -0,0 +1,79 @@
+import { execaCommand } from 'execa'
+import path, { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { cp, mkdir, readdir, readFile, rm, writeFile } from 'node:fs/promises'
+import got from 'got'
+import { pipeline } from 'node:stream/promises'
+import { createReadStream, createWriteStream } from 'node:fs'
+import { createGunzip } from 'node:zlib'
+import tar from 'tar-fs'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const root = path.join(__dirname, '..')
+
+const URL = 'https://registry.npmjs.org/@types/node/-/node-20.4.1.tgz'
+
+const getTestName = (line) => {
+  return (
+    'types-node-' +
+    line
+      .toLowerCase()
+      .trim()
+      .replaceAll(' ', '-')
+      .replaceAll('/', '-')
+      .replace('.d.ts', '')
+  )
+}
+
+const getAllTests = async (folder) => {
+  const dirents = await readdir(folder, { recursive: true })
+  const allTests = []
+  for (const dirent of dirents) {
+    if (!dirent.endsWith('.d.ts')) {
+      continue
+    }
+    const filePath = join(folder, dirent)
+    const fileContent = await readFile(filePath, 'utf8')
+    const testName = getTestName(dirent)
+    allTests.push({
+      testName,
+      testContent: fileContent,
+    })
+  }
+  return allTests
+}
+
+const writeTestFiles = async (allTests) => {
+  for (const test of allTests) {
+    await writeFile(`${root}/test/cases/${test.testName}.ts`, test.testContent)
+  }
+}
+
+const download = async (url, outPath) => {
+  await pipeline(got.stream(url), createWriteStream(outPath))
+}
+
+const extractTarGz = async (inFile, outDir) => {
+  await mkdir(outDir, { recursive: true })
+  await pipeline(
+    createReadStream(inFile),
+    createGunzip(),
+    tar.extract(outDir, {
+      strip: 1,
+    })
+  )
+}
+
+const main = async () => {
+  process.chdir(root)
+  await rm(`${root}/.tmp`, { recursive: true, force: true })
+  await mkdir(`${root}/.tmp`, { recursive: true })
+  await download(URL, `${root}/.tmp/types-node.tgz`)
+  await extractTarGz(`${root}/.tmp/types-node.tgz`, `${root}/.tmp/types-node`)
+  await rm(`${root}/.tmp/types-node/ts4.8`, { recursive: true, force: true })
+  process.chdir(root)
+  const allTests = await getAllTests(`${root}/.tmp/types-node`)
+  await writeTestFiles(allTests)
+}
+
+main()
