0cb34eca3bfe1a3e5704f5ff43198a9725831dbb
Le Vivilet <levivlet@pm.me>
2023-07-13 22:34:55 +0200

    feature: improve highlighting for regexp class

--- src/tokenizeTypeScript.js
+++ src/tokenizeTypeScript.js
@@ -187,7 +187,7 @@ const RE_KEYWORD_READONLY = /^readonly\b/
 const RE_SHEBANG = /^\#\!\/.*/
 const RE_SPREAD = /^\.\.\./
 const RE_BUILTIN_CLASS =
-  /^(?:Array|Object|Promise|ArrayBuffer|URL|URLSearchParams|WebSocket|FileSystemHandle|Function|StorageEvent|MessageEvent|MessageChannel|Int32Array|Boolean|String|Error|Set)\b/
+  /^(?:Array|Object|Promise|ArrayBuffer|URL|URLSearchParams|WebSocket|FileSystemHandle|Function|StorageEvent|MessageEvent|MessageChannel|Int32Array|Boolean|String|Error|Set|RegExp)\b/
 
 const RE_KEYWORD_IMPLEMENTS = /^implements/
 
