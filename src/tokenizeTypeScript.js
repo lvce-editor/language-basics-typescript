7e71ff51cd5e5198579b74df46c0e69da2e10987
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-07-17 13:06:31 +0200

    feature: improve highlighting for accessor keyword (#185)

--- src/tokenizeTypeScript.js
+++ src/tokenizeTypeScript.js
@@ -120,7 +120,7 @@ export const initialLineState = {
 
 const RE_LINE_COMMENT = /^\/\/[^\n]*/
 const RE_KEYWORD =
-  /^(?:yield|with|while|void|var|undefined|typeof|type|true|try|throw|this|static|switch|super|readonly|return|public|protected|private|package|null|new|let|interface|instanceof|in|import|implements|if|function|for|finally|from|false|extends|export|enum|else|do|delete|default|debugger|declare|continue|const|constructor|class|catch|case|break|await|async|abstract)\b/
+  /^(?:yield|with|while|void|var|undefined|typeof|type|true|try|throw|this|static|switch|super|readonly|return|public|protected|private|package|null|new|let|interface|instanceof|in|import|implements|if|function|for|finally|from|false|extends|export|enum|else|do|delete|default|debugger|declare|continue|const|constructor|class|catch|case|break|await|async|accessor|abstract)\b/
 
 const RE_WHITESPACE = /^\s+/
 const RE_VARIABLE_NAME = /^[\#\$a-zA-Z\_][\$a-zA-Z\_\d]*/
@@ -155,7 +155,7 @@ const RE_ANYTHING_UNTIL_END = /^.+/s
 const RE_CURLY_OPEN = /^\{/
 const RE_CURLY_CLOSE = /^\}/
 const RE_KEYWORD_CLASS_PROPERTY_MODIFIER =
-  /^(?:override|public|protected|private|readonly)\b/
+  /^(?:override|public|protected|private|readonly|accessor)\b/
 
 const RE_VERTICAL_LINE = /^\|/
 const RE_ROUND_OPEN = /^\(/
