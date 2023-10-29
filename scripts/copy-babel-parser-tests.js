287ecf1c9a3c2d45855f3b600c27a7bc6d397717
Le Vivilet <levivlet@pm.me>
2023-10-29 15:05:03 +0100

    feature: add babel parser tests

---
+++ scripts/copy-babel-parser-tests.js
@@ -0,0 +1,69 @@
+import { execaCommand } from 'execa'
+import { cp, readFile, readdir, rm, writeFile } from 'node:fs/promises'
+import path, { basename, dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const root = path.join(__dirname, '..')
+
+const REPO = 'https://github.com/babel/babel'
+const COMMIT = 'c446ff85c28e117ebf3cd72cd34ef358f1077aa8'
+
+const getTestName = (baseName) => {
+  return (
+    'babel-parser-' +
+    baseName
+      .toLowerCase()
+      .trim()
+      .replaceAll(' ', '-')
+      .replaceAll('/', '-')
+      .replaceAll(',', '')
+      .replaceAll('_', '-')
+  )
+}
+
+const getAllTests = async (folder) => {
+  const dirents = await readdir(folder, { recursive: true })
+  const allTests = []
+  for (const dirent of dirents) {
+    if (!dirent.endsWith('.ts') || dirent.endsWith('.d.ts')) {
+      continue
+    }
+    const filePath = `${folder}/${dirent}`
+    const fileContent = await readFile(filePath, 'utf8')
+    const testName = getTestName(dirent)
+    allTests.push({
+      testName,
+      testContent: fileContent,
+    })
+  }
+  return allTests
+}
+
+const writeTestFiles = async (allTests) => {
+  for (const test of allTests) {
+    await writeFile(`${root}/test/cases/${test.testName}`, test.testContent)
+  }
+}
+
+const main = async () => {
+  process.chdir(root)
+  await rm(`${root}/.tmp`, { recursive: true, force: true })
+  await execaCommand(`git clone ${REPO} .tmp/babel`, {
+    stdio: 'inherit',
+  })
+  process.chdir(`${root}/.tmp/babel`)
+  await execaCommand(`git checkout ${COMMIT}`)
+  process.chdir(root)
+  await cp(
+    `${root}/.tmp/babel/packages/babel-parser/src`,
+    `${root}/.tmp/babel-parser-src`,
+    {
+      recursive: true,
+    }
+  )
+  const allTests = await getAllTests(`${root}/.tmp/babel-parser-src`)
+  await writeTestFiles(allTests)
+}
+
+main()
