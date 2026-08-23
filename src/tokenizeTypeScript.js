155b17d5df798785df420c11d91208ac4142da61
Le Vivilet <levivilet@pm.me>
2026-08-22 23:29:43 -0700

    fix: highlight conditional object spreads (#353)

--- src/tokenizeTypeScript.js
+++ src/tokenizeTypeScript.js
@@ -2310,6 +2310,9 @@ export const tokenizeLine = (line, lineState) => {
           stack.push(state)
           token = TokenType.Comment
           state = State.InsideBlockComment
+        } else if ((next = part.match(RE_AMPERSAND))) {
+          token = TokenType.Punctuation
+          state = State.InsideReturnObject
         } else if ((next = part.match(RE_ANYTHING_UNTIL_END))) {
           token = TokenType.Text
           state = State.InsideReturnObject
