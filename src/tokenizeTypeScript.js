ad5837717c3e851e033f986ed572cef90ac166e7
Le Vivilet <levivilet@pm.me>
2026-08-25 14:32:44 -0700

    Fix primitive generic type highlighting (#364)

--- src/tokenizeTypeScript.js
+++ src/tokenizeTypeScript.js
@@ -1084,6 +1084,9 @@ export const tokenizeLine = (line, lineState) => {
           token = TokenType.Punctuation
           state = State.BeforeType
           stack.push(State.AfterType)
+        } else if ((next = part.match(RE_TYPE_PRIMITIVE))) {
+          token = TokenType.TypePrimitive
+          state = State.AfterType
         } else if ((next = part.match(RE_VARIABLE_NAME))) {
           token = TokenType.Type
           state = State.AfterType
