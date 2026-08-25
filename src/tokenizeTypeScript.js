91b83d33607f82cf74eae215bf86da4126e492c9
Le Vivilet <levivilet@pm.me>
2026-08-25 20:10:49 +0000

    fix: highlight arrow function union types

--- src/tokenizeTypeScript.js
+++ src/tokenizeTypeScript.js
@@ -332,16 +332,14 @@ const highlightNamedArrowFunctionTypes = (line, tokens) => {
 const IDENTIFIER_PATTERN = '[\\#\\$a-zA-Z\\_][\\$a-zA-Z\\_\\d]*'
 const SIMPLE_GENERIC_ARGUMENT_PATTERN = `(?:readonly\\s+)?${IDENTIFIER_PATTERN}(?:\\[\\])*`
 const GENERIC_ARGUMENTS_PATTERN = `<\\s*${SIMPLE_GENERIC_ARGUMENT_PATTERN}(?:\\s*,\\s*${SIMPLE_GENERIC_ARGUMENT_PATTERN})*\\s*>`
-const SIMPLE_TYPE_PATTERN = `(?:readonly\\s+)?${IDENTIFIER_PATTERN}(?:${GENERIC_ARGUMENTS_PATTERN})?(?:\\[\\])*`
+const SIMPLE_TYPE_MEMBER_PATTERN = `(?:readonly\\s+)?${IDENTIFIER_PATTERN}(?:${GENERIC_ARGUMENTS_PATTERN})?(?:\\[\\])*`
+const SIMPLE_TYPE_PATTERN = `${SIMPLE_TYPE_MEMBER_PATTERN}(?:\\s*[|&]\\s*${SIMPLE_TYPE_MEMBER_PATTERN})*`
 const SIMPLE_PARAMETER_PATTERN = `(?:\\.\\.\\.)?${IDENTIFIER_PATTERN}(?:\\??\\s*:\\s*${SIMPLE_TYPE_PATTERN})?`
 const RE_SIMPLE_ARROW_FUNCTION = new RegExp(
   `(?:export\\s+)?(?:const|let|var)\\s+${IDENTIFIER_PATTERN}\\s*=\\s*(?:async\\s+)?\\((\\s*(?:${SIMPLE_PARAMETER_PATTERN}(?:\\s*,\\s*${SIMPLE_PARAMETER_PATTERN})*\\s*,?)?\\s*)\\)\\s*(?::\\s*(${SIMPLE_TYPE_PATTERN}))?\\s*=>`,
   'g'
 )
-const RE_PARAMETER_TYPE = new RegExp(
-  `:\\s*(?:readonly\\s+)?(${IDENTIFIER_PATTERN})\\b(?:\\s*<\\s*(${SIMPLE_GENERIC_ARGUMENT_PATTERN}(?:\\s*,\\s*${SIMPLE_GENERIC_ARGUMENT_PATTERN})*)\\s*>)?`,
-  'g'
-)
+const RE_PARAMETER_TYPE = new RegExp(`:\\s*(${SIMPLE_TYPE_PATTERN})`, 'g')
 const RE_TYPE_NAME = new RegExp(IDENTIFIER_PATTERN, 'g')
 
 const getTypeToken = (typeName) => {
@@ -363,24 +361,18 @@ const getArrowFunctionTypeOffsets = (line) => {
     const parameters = match[1]
     const parametersOffset = match.index + match[0].indexOf(parameters)
     for (const parameterMatch of parameters.matchAll(RE_PARAMETER_TYPE)) {
-      const typeName = parameterMatch[1]
+      const parameterType = parameterMatch[1]
       const parameterOffset = parametersOffset + parameterMatch.index
-      offsets.set(
-        parameterOffset + parameterMatch[0].indexOf(typeName),
-        getTypeToken(typeName)
-      )
-      const genericTypeNames = parameterMatch[2]
-      if (genericTypeNames) {
-        const genericTypesOffset =
-          parameterOffset + parameterMatch[0].indexOf(genericTypeNames)
-        for (const genericTypeMatch of genericTypeNames.matchAll(
-          RE_TYPE_NAME
-        )) {
-          offsets.set(
-            genericTypesOffset + genericTypeMatch.index,
-            getTypeToken(genericTypeMatch[0])
-          )
+      const parameterTypeOffset =
+        parameterOffset + parameterMatch[0].indexOf(parameterType)
+      for (const typeMatch of parameterType.matchAll(RE_TYPE_NAME)) {
+        if (typeMatch[0] === 'readonly') {
+          continue
         }
+        offsets.set(
+          parameterTypeOffset + typeMatch.index,
+          getTypeToken(typeMatch[0])
+        )
       }
     }
     const returnType = match[2]
