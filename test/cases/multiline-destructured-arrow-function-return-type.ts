39ffd384b565c1d3383b990608ad6e48a35a1001
Le Vivilet <levivilet@pm.me>
2026-09-03 13:39:14 +0000

    fix: highlight multiline arrow return types

---
+++ test/cases/multiline-destructured-arrow-function-return-type.ts
@@ -0,0 +1,11 @@
+export const create = ({
+  headerHeight = 0,
+  itemHeight,
+  minimumSliderSize = 20,
+}: {
+  readonly itemHeight: number
+  readonly headerHeight: number
+  readonly minimumSliderSize: number
+}): any => {
+  return {}
+}
