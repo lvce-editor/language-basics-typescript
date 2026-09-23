df32b740381986516cad0c4c57b619aa736de155
Le Vivilet <levivilet@pm.me>
2026-09-23 07:12:40 -0700

    fix: highlight Uint32Array as a class (#424)

---
+++ test/cases/arrow-function-uint32array-return-type.ts
@@ -0,0 +1,7 @@
+export const findRegexMatches = (): Uint32Array => {
+  return new Uint32Array([])
+}
+
+export const getCustomArray = (): Uint32ArrayCustom => {
+  return new Uint32ArrayCustom()
+}
