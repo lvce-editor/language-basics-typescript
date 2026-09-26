f7f46d190cf622ab1c7fc0c456a62eb16fad53cd
Le Vivilet <levivilet@pm.me>
2026-09-26 11:55:15 -0700

    fix: highlight async destructured parameter defaults (#428)

---
+++ test/cases/status-bar-worker-get-builtin-status-bar-items.ts
@@ -0,0 +1,25 @@
+import type { EditorStatus } from '../EditorStatus/EditorStatus.ts'
+import type { StatusBarItem } from '../StatusBarItem/StatusBarItem.ts'
+import { getEditorStatusBarItems } from '../GetEditorStatusBarItems/GetEditorStatusBarItems.ts'
+import { getNotificationsStatusBarItem } from '../GetNotificationsStatusBarItem/GetNotificationsStatusBarItem.ts'
+import { getProblemsStatusBarItem } from '../GetProblemsStatusBarItem/GetProblemsStatusBarItem.ts'
+
+interface GetBuiltinStatusBarItemsOptions {
+  readonly editorStatus?: EditorStatus
+  readonly notificationCount?: number
+  readonly notificationsEnabled?: boolean
+  readonly problemsEnabled?: boolean
+}
+
+export const getBuiltinStatusBarItems = async (
+  errorCount: number,
+  warningCount: number,
+  { editorStatus, notificationCount = 0, notificationsEnabled = true, problemsEnabled = true }: GetBuiltinStatusBarItemsOptions = {},
+): Promise<readonly StatusBarItem[]> => {
+  return [
+    ...getEditorStatusBarItems(editorStatus),
+    ...getNotificationsStatusBarItem(notificationsEnabled, notificationCount),
+    ...getProblemsStatusBarItem(errorCount, warningCount, problemsEnabled),
+  ]
+}
+const nextValue = 123
